f7e8b0d5a5289126efd471662b87db6451283973
deezwurld.net <120104289+deezwurld@users.noreply.github.com>
2022-12-12 06:54:00 -0800

    console concept

---
+++ msftRegister.cs
@@ -0,0 +1,5 @@
+console.writeline"AlabnaianGumiWorm7.5oz = 2.99F";
+Console.readkey();
+/*The PseduoConcept is to connect the scanning hardware or flatbed
+to the sofware. Items scanned correlates to user_input (variable). The console 
+output(receipt) should return inputItems in a list with price*/
